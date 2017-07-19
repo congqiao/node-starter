e2480a6a6fe3213744624447ffcc31e7602fa9df
Cong Qiao <congqiao@gmail.com>
2017-07-19 01:13:24 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  devtool: 'source-map',
+  target: 'node',
+}
